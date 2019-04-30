bf9104ab7ca81dae92e4cfdee5d2622a59cfeb60
rmalandra01 <50123711+rmalandra01@users.noreply.github.com>
2019-04-29 18:08:41 -0600

    Add files via upload

---
+++ user.js
@@ -0,0 +1,279 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6ilCXGZpEyK":
+        Script1();
+        break;
+      case "6K1hal42Dmv":
+        Script2();
+        break;
+      case "6Ngvsv1woNZ":
+        Script3();
+        break;
+      case "6NzqqyzVJMO":
+        Script4();
+        break;
+      case "6ZTScuOeSFn":
+        Script5();
+        break;
+      case "6AtTIShx9hG":
+        Script6();
+        break;
+      case "5hmcXriXOhh":
+        Script7();
+        break;
+      case "5ipq6JsfXux":
+        Script8();
+        break;
+      case "6etcriKjGCt":
+        Script9();
+        break;
+      case "6ZDiu1APQCT":
+        Script10();
+        break;
+      case "69WZoNSyoxi":
+        Script11();
+        break;
+      case "5wrvlEdy63m":
+        Script12();
+        break;
+      case "6B5e8guZMCt":
+        Script13();
+        break;
+      case "6pFUy7xFBZJ":
+        Script14();
+        break;
+      case "6OYThxpXMdi":
+        Script15();
+        break;
+      case "6D0X0y5lP0i":
+        Script16();
+        break;
+      case "6TeyyPOGlm8":
+        Script17();
+        break;
+      case "6W6HYNzanNf":
+        Script18();
+        break;
+      case "6E4ne7RgRBM":
+        Script19();
+        break;
+      case "6Fh7A0Zxzyx":
+        Script20();
+        break;
+      case "5hFiqiLN64u":
+        Script21();
+        break;
+      case "6ESEIWZpqwx":
+        Script22();
+        break;
+      case "5V89ZgpRacS":
+        Script23();
+        break;
+      case "63AW0j07Fld":
+        Script24();
+        break;
+      case "6rmNNBtrKDS":
+        Script25();
+        break;
+      case "5kCX12iT0mC":
+        Script26();
+        break;
+      case "6H8xFPm7YhX":
+        Script27();
+        break;
+      case "6BXHGo7rpYN":
+        Script28();
+        break;
+      case "6Wuo92wTEGC":
+        Script29();
+        break;
+      case "6GXJsBPVP1f":
+        Script30();
+        break;
+      case "6pEfMb6T2Nw":
+        Script31();
+        break;
+      case "5qZiv4CPWmQ":
+        Script32();
+        break;
+      case "6j9L2Hco2El":
+        Script33();
+        break;
+      case "5ZJyMZknEvQ":
+        Script34();
+        break;
+  }
+}
+
+function Script1()
+{
+  $(".slide-control-button-next").css("opacity", "0.4")
+}
+
+function Script2()
+{
+  $(".slide-control-button-next").css("opacity", "1")
+}
+
+function Script3()
+{
+  $(".slide-control-button-next").css("opacity", "0.4")
+}
+
+function Script4()
+{
+  $(".slide-control-button-next").css("opacity", "1")
+}
+
+function Script5()
+{
+  $(".slide-control-button-next").css("opacity", "0.4")
+}
+
+function Script6()
+{
+  $(".slide-control-button-next").css("opacity", "1")
+}
+
+function Script7()
+{
+  $(".slide-control-button-next").css("opacity", "0.4")
+}
+
+function Script8()
+{
+  $(".slide-control-button-next").css("opacity", "1")
+}
+
+function Script9()
+{
+  $(".slide-control-button-next").css("opacity", "0.4")
+}
+
+function Script10()
+{
+  $(".slide-control-button-next").css("opacity", "1")
+}
+
+function Script11()
+{
+  $(".slide-control-button-next").css("opacity", "0.4")
+}
+
+function Script12()
+{
+  $(".slide-control-button-next").css("opacity", "1")
+}
+
+function Script13()
+{
+  $(".slide-control-button-next").css("opacity", "0.4")
+}
+
+function Script14()
+{
+  $(".slide-control-button-next").css("opacity", "1")
+}
+
+function Script15()
+{
+  $(".slide-control-button-next").css("opacity", "0.4")
+}
+
+function Script16()
+{
+  $(".slide-control-button-next").css("opacity", "1")
+}
+
+function Script17()
+{
+  $(".slide-control-button-next").css("opacity", "0.4")
+}
+
+function Script18()
+{
+  $(".slide-control-button-next").css("opacity", "1")
+}
+
+function Script19()
+{
+  $(".slide-control-button-next").css("opacity", "0.4")
+}
+
+function Script20()
+{
+  $(".slide-control-button-next").css("opacity", "1")
+}
+
+function Script21()
+{
+  $(".slide-control-button-next").css("opacity", "0.4")
+}
+
+function Script22()
+{
+  $(".slide-control-button-next").css("opacity", "1")
+}
+
+function Script23()
+{
+  $(".slide-control-button-next").css("opacity", "0.4")
+}
+
+function Script24()
+{
+  $(".slide-control-button-next").css("opacity", "1")
+}
+
+function Script25()
+{
+  $(".slide-control-button-next").css("opacity", "0.4")
+}
+
+function Script26()
+{
+  $(".slide-control-button-next").css("opacity", "1")
+}
+
+function Script27()
+{
+  $(".slide-control-button-next").css("opacity", "0.4")
+}
+
+function Script28()
+{
+  $(".slide-control-button-next").css("opacity", "1")
+}
+
+function Script29()
+{
+  $(".slide-control-button-next").css("opacity", "0.4")
+}
+
+function Script30()
+{
+  $(".slide-control-button-next").css("opacity", "1")
+}
+
+function Script31()
+{
+  $(".slide-control-button-next").css("opacity", "0.4")
+}
+
+function Script32()
+{
+  $(".slide-control-button-next").css("opacity", "1")
+}
+
+function Script33()
+{
+  $(".slide-control-button-next").css("opacity", "0.4")
+}
+
+function Script34()
+{
+  $(".slide-control-button-next").css("opacity", "1")
+}
+
